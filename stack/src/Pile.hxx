4cf40a38aa960c5d97f52fe0b1d9ca95acc25f33
Matth-L <matthias.lapu@gmail.com>
2025-01-21 20:00:25 -0500

    Finished Stack Class

--- stack/src/Pile.hxx
+++ stack/src/Pile.hxx
@@ -2,108 +2,169 @@
 #define PILE_H
 
 #include <iostream>
+#include <memory>
 #include <stdexcept>
 #include <algorithm>
+
+using namespace std;
 
 template <typename T>
 class Pile
 {
 private:
-    T *elements;
-    int max_elements;
-    int taille_pile;
+    // "smart-pointer" permettant de ne pas s'occupe de la suppression
+    unique_ptr<T[]> tab;
+    // indice de l'élément au sommet de la pile
+    int ptrSommet;
+    // sommet
+    int tailleMax;
+    static const int MAX_PILE = 100;
 
 public:
     /**********************************************
-     * @brief
+     * @brief Constructeur de la pile
      *
-     * @param max
+     * @param max taille maximale de la pile
+     * @throws std::invalid_argument si max <= 0
+     * @throws std::bad_alloc si l'allocation de mémoire échoue
      ***********************************************/
-    Pile(int max);
+    Pile(int max)
+        : tab(make_unique<T[]>(max)),
+          ptrSommet(-1),
+          tailleMax(max)
+    {
+        if (max <= 0 || max > MAX_PILE)
+        {
+            throw invalid_argument("Pile, taille non autorisé");
+        }
+    }
 
     /**********************************************
-     * @brief
+     * @brief Constructeur de copie
      *
      ***********************************************/
-    Pile(const Pile &);
+    Pile(const Pile &to_copy)
+        : tab(make_unique<T[]>(to_copy.tailleMax)),
+          ptrSommet(to_copy.ptrSommet),
+          tailleMax(to_copy.tailleMax)
+    {
+        for (int i = 0; i <= ptrSommet; ++i)
+        {
+            tab[i] = to_copy.tab[i];
+        }
+    }
 
     /**********************************************
-     * @brief
+     * @brief Destructeur de la pile
      *
      ***********************************************/
-    ~Pile();
+    ~Pile() = default;
 
     /**********************************************
-     * @brief
+     * @brief Empile un élément dans la pile de type T
      *
      ***********************************************/
-    void empiler(const T &);
+    void empiler(const T &element)
+    {
+        if (ptrSommet + 1 >= tailleMax)
+        {
+            throw length_error("Empiler : la pile est pleine!");
+        }
+
+        tab[++ptrSommet] = element;
+    }
 
     /**********************************************
-     * @brief
+     * @brief Dépile un élément de type T de la pile
      *
      * @return T
      ***********************************************/
-    T depiler();
+    T depiler()
+    {
+        if (estVide())
+        {
+            throw logic_error("Dépiler : la pile est vide!");
+        }
+
+        return tab[ptrSommet--];
+    }
 
     /**********************************************
-     * @brief
+     * @brief Vérifie si la pile est vide
      *
-     * @return true
-     * @return false
+     * @return true si la pile est vide, false sinon
      ***********************************************/
-    bool estVide() const;
+    bool estVide() const
+    {
+        return ptrSommet == -1;
+    }
 
     /**********************************************
-     * @brief
+     * @brief Retourne la taille de la pile
      *
-     * @return int
+     * @return int la taille de la pile
      ***********************************************/
-    int taille() const;
-
+    int taille() const
+    {
+        return ptrSommet + 1;
+    }
     /**********************************************
-     * @brief
+     * @brief Retourne l'élément au sommet de la pile
      *
      * @return const T&
+     * @throws logic_error, s'il n'y a rien dans la pile
      ***********************************************/
-    const T &sommet() const;
+    const T &sommet() const
+    {
+        if (estVide())
+        {
+            throw logic_error("sommet() : il n'y a rien dans la pile");
+        }
+        return tab[ptrSommet];
+    }
 
     /**********************************************
-     * @brief
+     * @brief Surcharge de l'opérateur =
      *
-     * @return const Pile<T>&
+     * @param p ,la pile à copier
+     * @return Une référence à la pile courante
      ***********************************************/
-    const Pile<T> &operator=(const Pile<T> &);
+    Pile<T> &operator=(Pile<T> p)
+    {
+        if (this == &p)
+        {
+            return *this;
+        }
+
+        Pile<T> tmp(p);
+        swap(tab, tmp.tab);
+        swap(ptrSommet, tmp.ptrSommet);
+        swap(tailleMax, tmp.tailleMax);
+
+        return *this;
+    }
 
     /**********************************************
-     * @brief
+     * @brief Surcharge de l'opérateur <<
      *
      * @tparam U
      * @return std::ostream&
      ***********************************************/
     template <typename U>
-    friend std::ostream &operator<<(std::ostream &, const Pile<U> &);
+    friend std::ostream &operator<<(std::ostream &output, const Pile<U> &p)
+    {
+        output << "[";
+        for (int i = 0; i <= p.ptrSommet; ++i)
+        {
+            output << p.tab[i];
+            if (i < p.ptrSommet)
+            {
+                output << ", ";
+            }
+        }
+        output << "].";
+        return output;
+    }
 };
-
-template <typename T>
-Pile<T>::Pile(int max) : max_elements(max), taille_pile(0)
-{
-    elements = new T[max_elements];
-}
-
-template <typename T>
-Pile<T>::Pile(const Pile<T> &to_copy)
-    : max_elements(to_copy.max_elements),
-      taille_pile(to_copy.taille_pile)
-{
-    elements = new T[max_elements];
-    std::copy(to_copy.elements, to_copy.elements + taille_pile, elements);
-}
-
-template <typename T>
-Pile<T>::~Pile()
-{
-    delete[] elements;
-}
 
 #endif // PILE_H
--- stack/src/main.cxx
+++ stack/src/main.cxx
@@ -1,10 +1,7 @@
 #include <iostream>
 #include "Pile.hxx"
 
-using namespace std;
-
 int main()
 {
-    Pile<int> a(10);
     return 0;
 }
