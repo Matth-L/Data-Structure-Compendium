8750d6fd9dfcb254b93dc4bdf93ea542b57be402
Matth-L <matthias.lapu@gmail.com>
2025-02-25 10:24:58 -0500

    v1

--- distributed_list/include/Node.hpp
+++ distributed_list/include/Node.hpp
@@ -1,11 +1,6 @@
-/**********************************************
- * @file Node.hpp
- * @brief File that allows to compute double linked list in each thread
- * @link https://cplusplus.com/reference/thread/thread/
- ***********************************************/
-
-#include <mutex>
 #include <iostream>
+#include <thread>
+#include <mutex>
 #include <condition_variable>
 
 using namespace std;
@@ -13,90 +8,72 @@ using namespace std;
 class Node
 {
 private:
-    const int id;
-    Node *prev;
-    Node *next;
+    int node_id;
     int sum;
-    mutex mutex_sum;
-    condition_variable cv;
-    bool ready;
+    int global_sum;
 
 public:
-    Node(int id) : id(id), prev(nullptr), next(nullptr), sum(0), ready(false) {}
+    Node *previous_node;
+    Node *next_node;
+    mutex mutex_sum;
+    condition_variable cv;
+    bool ready = false;
 
-    /**********************************************
-     * @brief Set the Next object
-     * @param next
-     ***********************************************/
-    void setNext(Node *next)
+    Node(int id) : node_id(id), previous_node(nullptr), next_node(nullptr), sum(0) {}
+
+    int getID() { return node_id; }
+    int getSum() { return sum; }
+    int getGlobalSum() { return global_sum; }
+
+    void set_neighbors(Node *p, Node *n)
     {
-        this->next = next;
+        previous_node = p;
+        next_node = n;
     }
 
-    /**********************************************
-     * @brief Set the Prev object
-     * @param prev
-     ***********************************************/
-    void setPrev(Node *prev)
+    void start_propagation()
     {
-        this->prev = prev;
-    }
-
-    /**********************************************
-     * @brief
-     * @param value
-     ***********************************************/
-    void send(int value)
-    {
-        // We lock the sum variable
-        unique_lock<mutex> lock(mutex_sum);
-
-        sum += value;
-        ready = true; // We got the sum value, release the mutex
-
-        // Unblocks all threads currently waiting for this condition.
-        // https://cplusplus.com/reference/condition_variable/condition_variable/notify_all/
-        cv.notify_all();
-    }
-
-    int receive()
-    {
-        unique_lock<mutex> lock(mutex_sum);
-
-        // wait causes the current thread to block until the condition
-        // variable is notified
-        // https://en.cppreference.com/w/cpp/thread/condition_variable/wait
-        cv.wait(lock,
-                [this]
-                { return ready; });
-
-        ready = false; // locking the mutex, it will be released when sending
-        return sum;
-    }
-
-    /**********************************************
-     * @brief Computes the sum across the nodes
-     *
-     * Calculates the sum of node IDs in a double-linked list.
-     * Communicates with the next and previous nodes.
-     ***********************************************/
-    void computeSum()
-    {
-        if (next)
+        if (previous_node)
         {
-            next->send(id);
-            sum = next->receive();
+            previous_node->start_propagation();
         }
         else
         {
-            sum = id;
+            send(node_id, this);
         }
+    }
 
-        if (prev)
+    void send_final_sum(int value)
+    {
+        global_sum = value;
+        if (previous_node)
         {
-            int prevSum = prev->receive();
-            sum += prevSum;
-            prev->send(sum);
+            previous_node->send_final_sum(value);
         }
+    }
+    void send(int value, Node *sender)
+    {
+        unique_lock<mutex> lock(mutex_sum);
+        sum += value; // Accumulate the sum
+
+        ready = true;
+        cv.notify_one();
+
+        if (next_node)
+        {
+            next_node->send(sum + next_node->getID(), this);
+        }
+        else
+        { // last one should have the final value
+            global_sum = sum;
+            send_final_sum(global_sum);
+        }
+    }
+
+    void compute_sum()
+    {
+        unique_lock<mutex> lock(mutex_sum);
+        cv.wait(lock, [this]
+                { return ready; });
     }
 };
--- distributed_list/src/main.cpp
+++ distributed_list/src/main.cpp
@@ -1,61 +1,48 @@
 #include <iostream>
 #include <thread>
-#include <random>
-#include <vector>
-#include <memory>
+#include <mutex>
+#include <condition_variable>
+
 #include "Node.hpp"
-
-void threadFunction(Node *node)
-{
-    node->computeSum();
-}
-
-using namespace std;
 
 int main()
 {
-    std::vector<std::unique_ptr<Node>> list_node;
-    size_t size = 100;
+    const int num_nodes = 5;
+    Node *nodes[num_nodes];
 
-    // Getting the seed for the rng
-    std::random_device device;
-    std::mt19937 rng(device());
-    std::uniform_int_distribution<> break_dist(1, 10);
-
-    // Create the first node
-    int id = break_dist(rng);
-    list_node.push_back(std::make_unique<Node>(id));
-
-    // Create the rest of the nodes
-    for (size_t i = 1; i < size; ++i)
+    // Création des nœuds
+    for (int i = 0; i < num_nodes; ++i)
     {
-        int id = break_dist(rng);
-        auto new_node = std::make_unique<Node>(id);
-
-        // Link the new node with the previous node
-        Node &previous_node = *list_node.back();
-        previous_node.setNext(new_node.get());
-        new_node->setPrev(&previous_node);
-
-        // Add the new node to the vector
-        list_node.push_back(std::move(new_node));
+        nodes[i] = new Node(i + 1);
     }
 
-    // Create threads for each node
-    std::vector<std::thread> list_thread;
-    for (auto &node : list_node)
+    // Configuration des voisins
+    for (int i = 0; i < num_nodes; ++i)
     {
-        list_thread.emplace_back(threadFunction, node.get());
+        nodes[i]->set_neighbors(i > 0 ? nodes[i - 1] : nullptr,
+                                i < num_nodes - 1 ? nodes[i + 1] : nullptr);
     }
 
-    // Join threads
-    for (std::thread &t : list_thread)
+    // Lancement des threads
+    thread threads[num_nodes];
+    for (int i = 0; i < num_nodes; ++i)
     {
-        t.join();
+        threads[i] = thread(&Node::compute_sum, nodes[i]);
     }
 
-    // Output the sum from any node (should be the same)
-    cout << "Sum of all node IDs: " << list_node.back()->receive() << endl;
+    // Démarrage de la propagation
+    nodes[3]->start_propagation();
+    for (int i = 0; i < num_nodes; ++i)
+    {
+        cout << nodes[i]->getGlobalSum() << endl;
+    }
+
+    // Attente de la fin des threads
+    for (int i = 0; i < num_nodes; ++i)
+    {
+        threads[i].join();
+        delete nodes[i];
+    }
 
     return 0;
 }
